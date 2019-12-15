80d1502dba878e495b9b7da0294162e317e3d258
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-15 09:39:18 +0300

    add abiturs module

--- src/app.module.js
+++ src/app.module.js
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AbitursModule } from './abiturs/abiturs.module';
 
 @Module({
-  imports: [],
+  imports: [AbitursModule],
   controllers: [AppController],
   providers: [AppService],
 })
