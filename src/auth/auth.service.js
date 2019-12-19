34dd25b1965fc12523c25015aaf79dee261c0f21
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-19 21:00:50 +0300

    hash

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,6 +1,7 @@
 import { Injectable, Dependencies } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import bcrypt from 'bcrypt';
 
 @Dependencies(UsersService, JwtService)
 @Injectable()
@@ -12,15 +13,18 @@ export class AuthService {
 
   async validateUser(username, pass) {
     const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
-      const { password, ...result } = user;
+    //console.log(bcrypt.hashSync(pass, 10));
+    //console.log(user);
+    if (user && bcrypt.compareSync(pass, user.hash)) {
+      const { hash, ...result } = user;
       return result;
     }
+
     return null;
   }
 
   async login(user) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.login, sub: user.idUser };
     return {
       access_token: this.jwtService.sign(payload),
     };
