8dc415bd3223c1442b00e4414d341d278ac11346
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-19 22:44:12 +0300

    fix mysql

--- src/abiturs/abiturs.service.js
+++ src/abiturs/abiturs.service.js
@@ -4,16 +4,38 @@ import { MySqlService } from '../my-sql/my-sql.service';
 @Injectable()
 @Dependencies(MySqlService)
 export class AbitursService {
-  constructor(mySqlService){
-      this.db = mySqlService;
-    }
+  constructor(mySqlService) {
+    this.db = mySqlService;
+  }
 
-    getAll(request){
-        return `Hello abitur!`;        
-    }
+  getAll(request) {
+    return `Hello abitur!`;
+  }
 
-    async getOne(id){
-        return this.db.querySQL("SELECT * from `tAbitur` where `idAbitur` = ?;", [id]);
-    }
+  async getOne(id) {
+    let result =  await this.db.querySQL('SELECT * from `tAbitur` where `idAbitur` = ?;', [
+      id,
+    ]);
+    return result[0][0];
+  }
 
+  async getPage(params, req) {
+    return this.db.getPage(
+      '(select idAbitur, Фамилия from tAbitur) as q1',
+      params.page,
+      params.size,
+      params.sort,
+      params.filter,
+    );
+  }
+
+  async getPageWihtoutFilter(params, req) {
+    //console.log('abiturs/page ', params, req.query);
+    return this.db.getPage(
+      '(select idAbitur, Фамилия from tAbitur) as q1',
+      params.page,
+      params.size,
+      params.sort,
+    );
+  }
 }
