af8daac36ad376bd6329001d93d5daf7713bcbb1
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-27 14:51:45 +0300

    orm

--- src/app.module.js
+++ src/app.module.js
@@ -5,9 +5,11 @@ import { AbitursModule } from './abiturs/abiturs.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MySqlModule } from './my-sql/my-sql.module';
+import { OrmModule } from './orm/orm.module';
+import { ServiceModule } from './service/service.module';
 
 @Module({
-  imports: [AbitursModule, AuthModule, UsersModule, MySqlModule],
+  imports: [AbitursModule, AuthModule, UsersModule, MySqlModule, OrmModule, ServiceModule],
   controllers: [AppController],
   providers: [AppService],
 })
