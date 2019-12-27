8bcd6cb238bd057551d0ed66567c420df13a0f01
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-27 14:52:24 +0300

    fix unknown user

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -14,8 +14,8 @@ export class AuthService {
   async validateUser(username, pass) {
     const user = await this.usersService.findOne(username);
     //console.log(bcrypt.hashSync(pass, 10));
-    console.log(user);
-    if (user && bcrypt.compareSync(pass, user.hash)) {
+    //console.log(user);
+    if (user.hash && bcrypt.compareSync(pass, user.hash)) {
       const { hash, ...result } = user;
       return result;
     }
